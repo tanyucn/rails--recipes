f79ef53fec979e3b07b121d906e91d810524c4d6
谭宇 <tanyucn@outlook.com>
2017-09-19 21:26:07 +0800

    时区设置

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  resource :user
+
   resources :events
 
   namespace :admin do
@@ -10,7 +12,7 @@ Rails.application.routes.draw do
   end
 
   get "/faq" => "pages#faq"
-  
+
   root "events#index"
 
 end
