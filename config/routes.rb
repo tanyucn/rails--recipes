04f42150876f523afb0e0a0f36cd69193fcacb1f
谭宇 <tanyucn@outlook.com>
2017-09-24 22:52:17 +0800

    批次编辑 (Bulk Editing)

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,18 @@ Rails.application.routes.draw do
     root "events#index"
     resources :events do
       resources :tickets, :controller => "event_tickets"
+
+      collection do
+        post :bulk_update
+      end
+       
     end
         resources :users do
           resource :profile, :controller => "user_profiles"
         end
   end
+
+
 
   get "/faq" => "pages#faq"
 
