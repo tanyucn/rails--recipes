679d3362b91a110336bf250450dc1a2e9b2ce5b6
谭宇 <tanyucn@outlook.com>
2017-09-25 22:44:18 +0800

    自订列表顺序-传统UI

--- config/routes.rb
+++ config/routes.rb
@@ -12,16 +12,18 @@ Rails.application.routes.draw do
     root "events#index"
     resources :events do
       resources :tickets, :controller => "event_tickets"
-
+      member do
+        post :reorder
+      end
       collection do
         post :bulk_update
       end
-       
     end
         resources :users do
           resource :profile, :controller => "user_profiles"
         end
   end
+
 
 
 
