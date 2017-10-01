c1e3075a25be3fd1a8df5e4b750d9a6b82d54582
谭宇 <tanyucn@outlook.com>
2017-10-01 15:21:37 +0800

    前台的活动状态筛选

--- app/models/event.rb
+++ app/models/event.rb
@@ -28,4 +28,8 @@ class Event < ApplicationRecord
   accepts_nested_attributes_for :tickets, :allow_destroy => true, :reject_if => :all_blank
   has_many :registrations, :dependent => :destroy
 
+
+  scope :only_public, -> { where( :status => "public" ) }
+  scope :only_available, -> { where( :status => ["public", "private"] ) }
+  
 end
